dda9941a5a3bfa96afff04742736c362aa5ad9aa
Risk-taker0501 <risk.taker2024@gmail.com>
2023-06-09 17:31:13 +0900

    format

--- src/components/DataTable/defaultWrappers/index.js
+++
@@ -1,5 +0,0 @@
-import Actions from './actions'
-import { Header, HeaderServer } from './header'
-import Body from './body'
-
-export { Actions, Header, Body, HeaderServer }
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import DataTable from './components/DataTable'
-import DataTableServer from './components/DataTableSever'
-import { Search, Range, Slider, Dropdown } from './components/DataTable/Filters'
-
-export { DataTable, DataTableServer, Range, Search, Slider, Dropdown }
