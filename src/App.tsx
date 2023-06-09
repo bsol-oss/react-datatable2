4e48131fc39db0626de7d6bc777a5ef1f5099cd1
Risk-taker0501 <risk.taker2024@gmail.com>
2023-06-09 22:35:15 +0900

    feat: make 3 components in Datatable

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@ import React from 'react';
 import { Box } from '@chakra-ui/react';
 
 import DataTable from './components/DataTable';
-import ThemeToggleButton from './components/ThemeToggleButton';
+import ThemeToggleButton from './components/Partials/ThemeToggleButton';
+import SearchWrapper from './components/SearchWrapper';
+import MainTable from './components/MainTable';
+import PageWrapper from './components/PageWrapper';
 
 function App(): JSX.Element {
   return (
     <Box>
-      <DataTable />
+      <DataTable>
+        <Box>
+          <SearchWrapper />
+          <MainTable />
+          <PageWrapper />
+        </Box>
+      </DataTable>
       <ThemeToggleButton pos="fixed" bottom="2" right="2" />
     </Box>
   );
