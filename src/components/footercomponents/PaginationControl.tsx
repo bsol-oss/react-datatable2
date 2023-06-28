9348687b5b7a7c251c0c3ef22528e7679d314abf
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-06-28 16:36:53 +0500

    removed unused code

--- src/components/footercomponents/PaginationControl.tsx
+++ src/components/footercomponents/PaginationControl.tsx
@@ -3,37 +3,24 @@ import { Select } from '@chakra-ui/react';
 import columns from '../ProvideByConsumer/Columns';
 import {
   ColumnResizeMode,
-  SortingState,
   getCoreRowModel,
   getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table';
 import {
   FilterContext,
-  PaginationContext,
 } from '../globalpartials/GlobalContext';
-import { getFilteredData } from '../../Data/Api';
 import { DataInterface } from '../../const/types';
 
 const PaginationControl = () => {
   const [data, setData] = useState<DataInterface[]>([]);
-  const [sorting, setSorting] = useState<SortingState>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const { setTotalCount } = useContext(PaginationContext);
   const { filterTerm, setFilterTerm } = useContext(FilterContext);
-  const [rowSelection, setRowSelection] = useState({});
+
   const columnResizeMode: ColumnResizeMode = 'onChange';
 
   const tableInstance = useReactTable({
     data,
     columns,
-    state: {
-      sorting,
-      rowSelection,
-    },
-    enableRowSelection: true,
-    onRowSelectionChange: setRowSelection,
-    onSortingChange: setSorting,
     columnResizeMode,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
@@ -46,19 +33,6 @@ const PaginationControl = () => {
       rows: tableInstance.getState().pagination.pageSize,
     });
   }, [tableInstance.getState().pagination.pageSize]);
-
-  useEffect(() => {
-    const fetchSubareas = async () => {
-      setIsLoading(true);
-      const res = await getFilteredData(filterTerm);
-      setIsLoading(false);
-      if (res) {
-        setTotalCount(res.filterCount);
-        setData(res.results);
-      }
-    };
-    fetchSubareas();
-  }, [filterTerm, tableInstance.getState().pagination.pageSize]);
 
   return (
     <Select
