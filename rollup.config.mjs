4506e7c9677d9e15820da4beae576bda5e7d68f6
jyotipadhi1 <jyotipadhi1@gmail.com>
2023-11-28 19:52:19 +0530

    rollup change

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,29 +2,23 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
+import json from '@rollup/plugin-json';
 
 export default [
   {
     input: 'src/index.ts',
     output: [
       {
-        file: 'lib/index.js',
+        file: 'dist/index.js',
         format: 'cjs',
         sourcemap: true,
       },
       {
-        file: 'lib/index.esm.js',
+        file: 'dist/index.esm.js',
         format: 'esm',
         sourcemap: true,
       },
     ],
-    plugins: [peerDepsExternal(), resolve(), commonjs(), typescript()],
-  },
-  {
-    input: 'lib/index.d.ts',
-    output: [{ file: 'lib/index.d.ts', format: 'es' }],
-    plugins: [dts()],
-    external: [/\.css$/],
+    plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(), json()],
   },
 ];
